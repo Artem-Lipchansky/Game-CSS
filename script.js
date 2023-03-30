9ddc0fcfd00d154006fb39725433eeb6c3a425e5
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-03-30 13:03:56 +0300

    added-game-over

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("keydown", function(event) {
     jump();
 });
 
-function jump (){
+function jump () {
     //dino.classList.add("jump")
 
     if (dino.classList != "jump"){
@@ -13,7 +13,16 @@ function jump (){
     }
 
     setTimeout( function() {
-        dino.classList.remote("jump")
+        dino.classList.remove("jump")
 }, 300)
 
 }
+
+let isAlive = setInterval(function() {
+ let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+ let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+ if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140 ) {
+    alert("GAME OVER!!")
+ }
+}, 10)
