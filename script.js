c886bccf4b56d13b2e5c7b1c37333d6f54719666
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-03-30 12:34:12 +0300

    added

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const dino = document .getElementById("dino");
-const cactus =const dino = document .getElementById("cactus");
+const cactus = document .getElementById("cactus");
 
-document.addEvent
+document.addEventListener("keydown", function(event) {
+    jump();
+});
+
+function jump (){
+    //dino.classList.add("jump")
+
+    if (dino.classList != "jump"){
+        dino.classList.add("jump")
+    }
+
+    setTimeout( function() {
+        dino.classList.remote("jump")
+}, 300)
+
+}
